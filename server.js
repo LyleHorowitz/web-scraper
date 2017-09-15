1024229f581216ab426881a3eb686a363bdbdf56
Lyle <5846films@gmail.com>
2017-09-14 20:54:00 -0700

    db is receiving scraped data, created mongoose connection/id

--- server.js
+++ server.js
@@ -1,198 +1,82 @@
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var cheerio = require("cheerio");
+// Require request and cheerio. This makes the scraping possible
 var request = require("request");
+var cheerio = require("cheerio");
 
+// Initialize Express
 var app = express();
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from reddit's AHS board:" +
-            "\n***********************************\n");
-
-// Making a request for reddit's "American Horror Story" board. The page's HTML is passed as the callback's third argument
-request("https://www.reddit.com/r/webdev", function(error, response, html) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
-
-  // An empty array to save the data that we'll scrape
-  var results = [];
-
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
-  $("p.title").each(function(i, element) {
-
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
-
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      link: link
-    });
-  });
-
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
-});
-
-// Set the app up with morgan, body-parser, and a static folder
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(express.static("public"));
 
 // Database configuration
-var databaseUrl = "postdb"; // "week18day2"
-var collections = ["posts"]; // "notes"
+var databaseUrl = "postdb";
+var collections = ["posts"];
 
-// Hook mongojs config to db variable
+// Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
-
-// Log any mongojs errors to console
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-
-// Routes
-// ======
-
-// Simple index route
+// Main route (simple Hello World Message)
 app.get("/", function(req, res) {
-  res.send(index.html);
+  res.send("Hello world");
 });
 
-// Handle form submission, save submission to mongo
-app.post("/submit", function(req, res) {
-  console.log(req.body);
-  // Insert the post into the posts collection
-  db.posts.insert(req.body, function(error, saved) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the note back to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      res.send(saved);
-    }
-  });
-});
-
-// Retrieve results from mongo
+// Retrieve data from the db
 app.get("/all", function(req, res) {
-  // Find all posts in the posts collection
-  db.posts.find({}, function(error, found) {
-    // Log any errors
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
     if (error) {
       console.log(error);
     }
-    // Otherwise, send json of the posts back to user
-    // This will fire off the success function of the ajax request
+    // If there are no errors, send the data to the browser as json
     else {
       res.json(found);
     }
   });
 });
 
-// Select just one note by an id
-app.get("/find/:id", function(req, res) {
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request
+  request("https://reddit.com/r/americanhorrorstory", function(error, response, html) {
+    console.log(html);
+    // Load the html body from request into cheerio
+    var $ = cheerio.load(html);
+    // For each element with a "title" class
+    $(".title").each(function(i, element) {
+      // Save the text and href of each link enclosed in the current element
+      var title = $(element).children("a").text();
+      var link = $(element).children("a").attr("href");
 
-  // When searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-
-  // Find just one result in the posts collection
-  db.posts.findOne({
-    // Using the id in the url
-    "_id": mongojs.ObjectId(req.params.id)
-  }, function(error, found) {
-    // log any errors
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the post to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(found);
-      res.send(found);
-    }
+      // If this found element had both a title and a link
+      if (title && link) {
+        // Insert the data in the scrapedData db
+        db.postdb.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            // Log the error if one is encountered during the query
+            console.log(err);
+          }
+          else {
+            // Otherwise, log the inserted data
+            console.log(inserted);
+          }
+        });
+      }
+   });
   });
+
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
 });
 
-  // Update the note that matches the object id
-  db.posts.update({
-    "_id": mongojs.ObjectId(req.params.id)
-  }, {
-    // Set the title, post and modified parameters
-    // sent in the req's body.
-    $set: {
-      "title": req.body.title, // // // // //
-      "post": req.body.post,
-      "modified": Date.now()
-    }
-  }, function(error, edited) {
-    // Log any errors from mongojs
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the mongojs response to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(edited);
-      res.send(edited);
-    }
-  });
-});
-
-// Delete One from the DB
-app.get("/delete/:id", function(req, res) {
-  // Remove a post using the objectID
-  db.posts.remove({
-    "_id": mongojs.ObjectID(req.params.id)
-  }, function(error, removed) {
-    // Log any errors from mongojs
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the mongojs response to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(removed);
-      res.send(removed);
-    }
-  });
-});
-
-// Clear the DB
-app.get("/clearall", function(req, res) {
-  // Remove every post from the posts collection
-  db.posts.remove({}, function(error, response) {
-    // Log any errors to the console
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the mongojs response to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(response);
-      res.send(response);
-    }
-  });
-});
+mongoose.connect("mongodb://heroku_3fnrdd84:af4r5u0kuh7cfkt4okdn6pjd6g@ds135624.mlab");
 
 // Listen on port 3000
 app.listen(3000, function() {
